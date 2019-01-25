26388c13031650ea969e1d2b59ad09c83c1d5759
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-25 08:42:43 +0300

    storage link built

--- app/Http/Controllers/Card/CardController.php
+++ app/Http/Controllers/Card/CardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Card;
 
+use App;
 use App\Models\Card\Card;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -10,6 +11,9 @@ class CardController extends Controller
 {
     public function index()
     {
+        if(!file_exists(public_path('storage'))) {
+           App::make('files')->link(storage_path('app/public'), public_path('storage'));
+        }
         return view('cards.index');
     }
 
