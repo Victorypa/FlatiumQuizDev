386d95abd60bbae1331f4329d6446b476329508e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-25 08:59:06 +0300

    make auth

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ Route::get('/', 'Form\FormController@index');
 Route::group(['prefix' => 'cards', 'namespace' => 'Card'], function () {
     Route::get('/', 'CardController@index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
