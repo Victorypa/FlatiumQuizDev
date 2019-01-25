e1cba381eea65435dd487cbe23d5754bd42ad545
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-26 01:20:34 +0300

    refactored to guzzle

--- app/Http/Controllers/Form/FormController.php
+++ app/Http/Controllers/Form/FormController.php
@@ -31,7 +31,7 @@ class FormController extends Controller
     public function store(Card $card, Request $request)
     {
         $person = $card->people()->create($request->all());
-        
+
         $this->create($person);
     }
 
@@ -50,31 +50,21 @@ class FormController extends Controller
     protected function create($person)
     {
         $this->auth();
-        $link= 'https://flatium.amocrm.ru/api/v2/leads';
 
-        $leads['add'] = array(
+        $link = 'https://flatium.amocrm.ru/api/v2/leads';
+
+        $lead['add'] = array(
             array(
                 'name' => $person->email . '  ' . $person->name,
                 'pipeline_id' => 1572109
             )
         );
 
-        $curl=curl_init(); #Save the cURL session descriptor
-        #Set the necessary parameters for the cURL session
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
-        curl_setopt($curl,CURLOPT_URL,$link);
-        curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
-        curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($leads));
-        curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-        curl_setopt($curl,CURLOPT_HEADER,false);
-        curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-        curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
-        $out=curl_exec($curl); #Initiate a request to the API and save the response to a variable
-        $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
-        /* Now we can process the response received from the server. This is an example. You can process the data in your own way. */
-        $code=(int)$code;
+        $response = $this->client->request('POST', $link, [
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+            'body' => json_encode($leads)
+        ]);
     }
 }
