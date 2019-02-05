cbbec08db2551e77867dea2ddd036cc041debc63
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-05 18:25:02 +0300

    bug left

--- app/Http/Controllers/Form/FormController.php
+++ app/Http/Controllers/Form/FormController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\Amo\Contact\ContactCreate;
 use App\Services\Amo\Leads\LeadCreate;
+use App\Services\Amo\Crud;
 
 class FormController extends Controller
 {
@@ -23,20 +24,22 @@ class FormController extends Controller
             'email' => $request->email
         ]);
 
-        $contact = (new ContactCreate($this->client))->create([
-            'name' => $person->name,
-            'email' => $person->name,
-            'phone' => $person->phone
-        ]);
-
-        if (!empty($contact)) {
-            (new LeadCreate($this->client))->create([
-                'name' => $person->name,
-                'tags' => $person->card->result,
-                'pipeline_id' => 1572109,
-                'contacts_id' => $contact->id,
-                'responsible_user_id' => 2211916
-            ]);
-        }
+        (new Crud($this->client, $person, $pipelineId))->create();
+        
+        // $contact = (new ContactCreate($this->client))->create([
+        //     'name' => $person->name,
+        //     'email' => $person->name,
+        //     'phone' => $person->phone
+        // ]);
+        //
+        // if (!empty($contact)) {
+        //     (new LeadCreate($this->client))->create([
+        //         'name' => $person->name,
+        //         'tags' => $person->card->result,
+        //         'pipeline_id' => 1572109,
+        //         'contacts_id' => $contact->id,
+        //         'responsible_user_id' => 2211916
+        //     ]);
+        // }
     }
 }
