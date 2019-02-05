62d843458b2676b9da74b38fa38bcab160e231ac
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-05 10:34:01 +0300

    tag manager scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,11 @@
             ]) !!};
         </script>
 
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPTLKSR"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MPTLKSR');</script>
     </head>
     <body>
         <div id="app">
@@ -28,10 +31,7 @@
         <script src="{{ asset('js/app.js') }}"></script>
         @yield('scripts')
 
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MPTLKSR');</script>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPTLKSR"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </body>
 </html>
