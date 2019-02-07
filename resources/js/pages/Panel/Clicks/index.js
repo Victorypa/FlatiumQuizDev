acbc75c48843d84bbcb05e689a5667d83a3fa241
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-07 14:13:54 +0300

    modal in clicks

---
+++ resources/js/pages/Panel/Clicks/index.js
@@ -0,0 +1,33 @@
+export const payload = [
+    {   'group': 'A',
+        'elements': ['A1', 'A2', 'A3']
+    },
+
+    {   'group': 'B',
+        'elements': ['B1', 'B2', 'B3']
+    },
+
+    {   'group': 'C1',
+        'elements': ['C11', 'C12', 'C13']
+    },
+
+    {   'group': 'C2',
+        'elements': ['C21', 'C22', 'C23']
+    },
+
+    {   'group': 'C3',
+        'elements': ['C31', 'C32', 'C33']
+    },
+
+    {   'group': 'D1',
+        'elements': ['D11', 'D12', 'D13']
+    },
+
+    {   'group': 'D2',
+        'elements': ['D21', 'D22', 'D23']
+    },
+
+    {   'group': 'D3',
+        'elements': ['D31', 'D32', 'D33']
+    }
+]
--- resources/js/plugins.js
+++ resources/js/plugins.js
@@ -1,6 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal)
 
 library.add(faCoffee)
 
