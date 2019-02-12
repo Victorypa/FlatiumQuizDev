a4145a60542c2ae6e8ba567dc78f2fe2ab79e681
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-12 12:18:40 +0300

    decorations component built

--- resources/js/pages/Calculate/Decoration/Decoration.vue
+++ resources/js/pages/Calculate/Decoration/Decoration.vue
@@ -11,153 +11,14 @@
             <h1 class="main-caption">
               Расскажите какая текущая  отделка в помещении
             </h1>
-
-            <div class="image-wrapper" v-for>
-
+            <div class="image-wrapper" v-if="decorations.length">
+              <div class="image-card fade-in" v-for="(decoration, index) in decorations">
+                  <button class="button-card" @click.prevent="selectDecoration(decoration.value)">
+                      <div v-html="decoration.svg"></div>
+                      <div class="image-text">{{ decoration.name }}</div>
+                  </button>
+              </div>
             </div>
-
-            <div class="image-wrapper">
-
-              <div class="image-card fade-in one">
-                  <button class="button-card">
-                    <svg width="112px" height="96px" viewBox="0 0 112 96" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                        <g id="Option-2---button-&amp;-talkback-screens" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                            <g id="Kind-of-Home" transform="translate(-306.000000, -370.000000)" stroke="#738494">
-                                <g id="home-kinds" transform="translate(280.000000, 339.000000)">
-                                    <g id="Group-7">
-                                        <g id="House">
-                                            <g transform="translate(27.000000, 32.000000)">
-                                                <path d="M30.8466708,90.8055185 C32.9130798,90.8055185 34.5882353,92.3059664 34.5882353,94.1568627 L7.68627451,94.1568627 C7.67623132,92.6194741 8.35614952,91.1429022 9.57335988,90.0589408 C10.7905702,88.9749795 12.4429835,88.3745422 14.1592932,88.3925506" id="Shape"></path>
-                                                <path d="M13.4509804,87.8620559 C14.685735,84.2933445 18.3910734,82.1208233 22.2054026,82.7291463 C26.0197319,83.3374693 28.8208761,86.5476677 28.8235294,90.3137255" id="Shape"></path>
-                                                <path d="M101.934258,90.7856897 C104.006439,90.7856897 105.686275,92.2950153 105.686275,94.1568627 L78.7843137,94.1568627 C78.8048065,90.9807616 81.665355,88.4105695 85.2002625,88.3921569" id="Shape"></path>
-                                                <path d="M84.5490196,87.8620559 C85.7837742,84.2933445 89.4891126,82.1208233 93.3034419,82.7291463 C97.1177711,83.3374693 99.9189153,86.5476677 99.9215686,90.3137255" id="Shape"></path>
-                                                <path d="M11.5294118,88.3921569 L11.5294118,38.4313725" id="Shape"></path>
-                                                <path d="M99.9215686,38.4313725 L99.9215686,84.5490196" id="Shape"></path>
-                                                <rect id="Rectangle-path" x="38.4313725" y="86.4705882" width="32.6666667" height="7.68627451"></rect>
-                                                <rect id="Rectangle-path" x="46.1176471" y="57.6470588" width="19.2156863" height="28.8235294"></rect>
-                                                <rect id="Rectangle-path" x="21.1372549" y="57.6470588" width="15.372549" height="15.372549"></rect>
-                                                <rect id="Rectangle-path" x="74.9411765" y="57.6470588" width="15.372549" height="15.372549"></rect>
-                                                <rect id="Rectangle-path" x="48.0392157" y="26.9019608" width="15.372549" height="13.4509804"></rect>
-                                                <path d="M55.7254902,1.92156863 L19.2156863,38.4313725" id="Shape"></path>
-                                                <path d="M55.7254902,1.92156863 L92.2352941,38.4313725" id="Shape"></path>
-                                                <path d="M82.627451,5.76470588 L59.5686275,5.76470588" id="Shape"></path>
-                                                <polyline id="Shape" points="92.7530821 38.4313725 103.764706 38.4313725 103.764706 5.76470588 92.2352941 5.76470588"></polyline>
-                                                <polyline id="Shape" points="19.2900186 38.4313725 7.68627451 38.4313725 7.68627451 5.76470588 51.8823529 5.76470588"></polyline>
-                                                <rect id="Rectangle-path" x="82.627451" y="0" width="9.60784314" height="13.4509804"></rect>
-                                                <path d="M80.7058824,0 L94.1568627,0" id="Shape"></path>
-                                                <path d="M0,94.1568627 L109.529412,94.1568627" id="Shape"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </svg>
-                      <div class="image-text">Новостройка без отделки</div>
-                  </button>
-              </div>
-
-              <div class="image-card fade-in two">
-                  <button class="button-card">
-                    <svg width="90px" height="92px" viewBox="0 0 90 92" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-
-                        <defs></defs>
-                        <g id="Option-2---button-&amp;-talkback-screens" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                            <g id="Kind-of-Home" transform="translate(-502.000000, -374.000000)" stroke="#738494">
-                                <g id="home-kinds" transform="translate(280.000000, 339.000000)">
-                                    <g id="Group-7-Copy" transform="translate(185.000000, 0.000000)">
-                                        <g id="Group-7">
-                                            <g id="Townhome" transform="translate(37.000000, 35.000000)">
-                                                <path d="M27.335,87.5 C29.2679966,87.5 30.835,89.0670034 30.835,91 L5.775,91 C5.77494371,87.708196 8.43325231,85.0342503 11.725,85.015" id="Shape"></path>
-                                                <path d="M11.725,84.98 C12.9766056,81.2669936 16.7292634,79.0031881 20.5974644,79.627651 C24.4656654,80.2521139 27.3153433,83.5817674 27.335,87.5" id="Shape"></path>
-                                                <path d="M81.655,87.5 C83.5879966,87.5 85.155,89.0670034 85.155,91 L60.06,91 C60.06,87.6945758 62.7395758,85.015 66.045,85.015" id="Shape"></path>
-                                                <path d="M66.045,84.98 C67.2988271,81.2608781 71.0613998,78.9967918 74.9346468,79.6307538 C78.8078938,80.2647157 81.6523057,83.6102143 81.655,87.535" id="Shape"></path>
-                                                <path d="M14.945,80.85 L14.945,25.025" id="Shape"></path>
-                                                <path d="M74.585,25.025 L74.585,79.59" id="Shape"></path>
-                                                <rect id="Rectangle-path" x="38.5" y="75.635" width="12.53" height="15.4"></rect>
-                                                <rect id="Rectangle-path" x="39.9" y="16.59" width="9.73" height="9.73"></rect>
-                                                <path d="M44.765,0.595 L19.985,25.025" id="Shape"></path>
-                                                <path d="M44.765,0.595 L69.545,25.025" id="Shape"></path>
-                                                <path d="M63.245,3.36 L47.6,3.36" id="Shape"></path>
-                                                <polyline id="Shape" points="69.545 25.025 77.35 25.025 77.35 3.36 63.245 3.36"></polyline>
-                                                <polyline id="Shape" points="19.985 25.025 12.18 25.025 12.18 3.36 41.93 3.36"></polyline>
-                                                <rect id="Rectangle-path" x="25.97" y="35.245" width="14.56" height="11.865"></rect>
-                                                <rect id="Rectangle-path" x="49.035" y="35.245" width="14.56" height="11.865"></rect>
-                                                <rect id="Rectangle-path" x="25.97" y="54.565" width="14.56" height="11.865"></rect>
-                                                <rect id="Rectangle-path" x="49.035" y="54.565" width="14.56" height="11.865"></rect>
-                                                <path d="M88.935,91 L0.595,91" id="Shape"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </svg>
-                    <div class="image-text">Новостройка с белыми стенами</div>
-                  </button>
-              </div>
-
-              <div class="image-card fade-in three">
-                  <button class="button-card">
-                    <svg width="114px" height="94px" viewBox="0 0 114 94" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <!-- Generator: Sketch 49.3 (51167) - http://www.bohemiancoding.com/sketch -->
-                      <defs></defs>
-                      <g id="Option-2---button-&amp;-talkback-screens" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                          <g id="Kind-of-Home" transform="translate(-676.000000, -371.000000)" stroke="#738494">
-                              <g id="home-kinds" transform="translate(280.000000, 339.000000)">
-                                  <g id="Group-7-Copy-2" transform="translate(370.000000, 0.000000)">
-                                      <g id="Group-7">
-                                          <g id="Apartment" transform="translate(26.000000, 32.000000)">
-                                              <path d="M25.020625,90.16 C26.711997,90.16 28.083125,91.531128 28.083125,93.2225 L6.125,93.2225 C6.125,90.3302538 8.4696288,87.985625 11.361875,87.985625" id="Shape"></path>
-                                              <path d="M11.361875,87.924375 C12.4589738,84.6701434 15.7512248,82.6890678 19.140316,83.2437846 C22.5294071,83.7985013 25.0182675,86.7258125 25.020625,90.16" id="Shape"></path>
-                                              <path d="M105.319375,90.16 C107.010747,90.16 108.381875,91.531128 108.381875,93.2225 L86.42375,93.2225 C86.42375,90.3302538 88.7683788,87.985625 91.660625,87.985625" id="Shape"></path>
-                                              <path d="M91.660625,87.924375 C92.7577238,84.6701434 96.0499748,82.6890678 99.439066,83.2437846 C102.828157,83.7985013 105.317017,86.7258125 105.319375,90.16" id="Shape"></path>
-                                              <path d="M0.520625,93.191875 L113.25125,93.191875" id="Shape"></path>
-                                              <polyline id="Shape" points="10.35125 88.046875 10.35125 16.72125 38.77125 16.72125 38.77125 93.191875"></polyline>
-                                              <rect id="Rectangle-path" x="38.77125" y="0.520625" width="36.19875" height="92.67125"></rect>
-                                              <path d="M28.665,28.0525 L21.100625,28.0525" id="Shape"></path>
-                                              <path d="M28.665,36.1375 L21.100625,36.1375" id="Shape"></path>
-                                              <path d="M28.665,44.2225 L21.100625,44.2225" id="Shape"></path>
-                                              <path d="M28.665,52.3075 L21.100625,52.3075" id="Shape"></path>
-                                              <path d="M28.665,60.3925 L21.100625,60.3925" id="Shape"></path>
-                                              <path d="M28.665,68.4775 L21.100625,68.4775" id="Shape"></path>
-                                              <path d="M28.665,76.5625 L21.100625,76.5625" id="Shape"></path>
-                                              <polyline id="Shape" points="75.000625 93.191875 75.000625 16.72125 103.420625 16.72125 103.420625 85.351875"></polyline>
-                                              <path d="M93.314375,28.0525 L85.719375,28.0525" id="Shape"></path>
-                                              <path d="M93.314375,36.1375 L85.719375,36.1375" id="Shape"></path>
-                                              <path d="M93.314375,44.2225 L85.719375,44.2225" id="Shape"></path>
-                                              <path d="M93.314375,52.3075 L85.719375,52.3075" id="Shape"></path>
-                                              <path d="M93.314375,60.3925 L85.719375,60.3925" id="Shape"></path>
-                                              <path d="M93.314375,68.4775 L85.719375,68.4775" id="Shape"></path>
-                                              <path d="M93.314375,76.5625 L85.719375,76.5625" id="Shape"></path>
-                                              <path d="M60.668125,14.056875 L53.10375,14.056875" id="Shape"></path>
-                                              <path d="M60.668125,22.141875 L53.10375,22.141875" id="Shape"></path>
-                                              <path d="M60.668125,30.226875 L53.10375,30.226875" id="Shape"></path>
-                                              <path d="M60.668125,38.311875 L53.10375,38.311875" id="Shape"></path>
-                                              <path d="M60.668125,46.396875 L53.10375,46.396875" id="Shape"></path>
-                                              <path d="M60.668125,54.481875 L53.10375,54.481875" id="Shape"></path>
-                                              <path d="M60.668125,62.566875 L53.10375,62.566875" id="Shape"></path>
-                                              <path d="M60.668125,70.651875 L53.10375,70.651875" id="Shape"></path>
-                                              <rect id="Rectangle-path" x="50.8375" y="81.095" width="12.096875" height="12.096875"></rect>
-                                          </g>
-                                      </g>
-                                  </g>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-                  <div class="image-text">Вторичное жилье со старой отделкой</div>
-                  </button>
-              </div>
-              <div class="button-wrapper">
-                  <button type="submit"
-                          class="primary-button"
-                          >
-                     Далее
-                 </button>
-               </div>
-            </div>
-
           </div>
         </div>
 </div>
@@ -165,7 +26,27 @@
 </template>
 
 <script>
+    import { decorations } from './index'
     export default {
+        data () {
+            return {
+                decorations
+            }
+        },
+
+        methods: {
+            selectDecoration (decoration) {
+                let card_id = window.location.search.match(/\d+/g).toString()
+
+                axios.post(`/cards/decoration/store`, {
+                    'card_id': card_id,
+                    'type': decoration
+                }).then(response => {
+                    
+                })
+            }
+        }
+
 
     }
 </script>
