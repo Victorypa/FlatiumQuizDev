9ef5219fc9e316f72ab802428bd32d76c7070e3d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-14 14:52:30 +0300

    all clicks finished

--- app/Http/Controllers/Card/Click/DesignSkillController.php
+++ app/Http/Controllers/Card/Click/DesignSkillController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Card\Click;
 
 use Illuminate\Http\Request;
+use App\Models\Card\Click\Click;
 use App\Http\Controllers\Controller;
 
 class DesignSkillController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('panel.clicks.design_skills.index', [
+            'junior' => Click::where('name', 'junior')->get()->count(),
+            'middle' => Click::where('name', 'middle')->get()->count(),
+            'senior' => Click::where('name', 'senior')->get()->count()
+        ]);
+    }
 }
--- resources/views/panel/clicks/design_skills/index.blade.php
+++ resources/views/panel/clicks/design_skills/index.blade.php
@@ -3,4 +3,51 @@
 @section('content')
     @include('panel.partials._navigation')
 
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <table class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>
+                                <strong>
+                                    Совсем не разбираюсь.
+                                </strong>
+                            </th>
+                            <th>
+                                <strong>
+                                    Кое-что понимаю.
+                                </strong>
+                            </th>
+                            <th>
+                                <strong>
+                                    Эксперт.
+                                </strong>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>
+                                    {{ $junior }}
+                                </strong>
+                            </td>
+                            <td>
+                                <strong>
+                                    {{ $middle }}
+                                </strong>
+                            </td>
+                            <td>
+                                <strong>
+                                    {{ $senior }}
+                                </strong>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 @endsection
