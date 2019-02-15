b3096e7efa7b6911e86f8d68697ac8952b720127
root <root@ovz2.maksim-sobolev.n0ykp.vps.myjino.ru>
2019-02-15 15:38:51 +0300

    renewed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,13 @@
 
         <title>Flatium</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        {{-- <link rel="shortcut icon" href="{{ secure_asset('images/favicon.ico') }}" type="image/x-icon" /> --}}
-        <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon" />
+        <link rel="shortcut icon" href="{{ secure_asset('images/favicon.ico') }}" type="image/x-icon" /> 
         <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
         {{-- <base href="https://test.flatium.ru/" /> --}}
 
-        {{-- <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}"> --}}
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}"> 
 
 
         <script>
@@ -35,9 +33,7 @@
             @yield('content')
         </div>
 
-        {{-- <script src="{{ secure_asset('js/app.js') }}"></script> --}}
-        <script src="{{ asset('js/app.js') }}"></script>
-
+        <script src="{{ secure_asset('js/app.js') }}"></script>
         @yield('scripts')
 
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPTLKSR"
