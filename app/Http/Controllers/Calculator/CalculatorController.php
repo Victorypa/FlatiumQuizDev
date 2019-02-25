094036b74b5055f43d498526ebb7380d6b6485ab
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-25 11:07:28 +0300

    roistat

--- app/Http/Controllers/Calculator/CalculatorController.php
+++ app/Http/Controllers/Calculator/CalculatorController.php
@@ -121,7 +121,8 @@ class CalculatorController extends Controller
                 'contacts_id' => $contact->id,
                 'responsible_user_id' => 2211916,
                 'custom_fields' => array(
-                    'square' => $request->get('square')
+                    'square' => $request->get('square'),
+                    'roistat_cookie' => $request->get('roistat_cookie') 
                 )
             ]);
         }
